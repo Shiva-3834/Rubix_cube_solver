814b75e58ca3b46282607006e48e110fc25e2d5e
Shiva-3834 <na21b030@smail.iitm.ac.in>
2024-04-25 10:19:38 +0530

    Added Generic Rubiks Cube Model

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dell on 25-04-2024.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,14 @@
+//
+// Created by Dell on 25-04-2024.
+//
+
+#ifndef PROJECTS_GENERICRUBIKSCUBE_H
+#define PROJECTS_GENERICRUBIKSCUBE_H
+
+
+class GenericRubiksCube {
+
+};
+
+
+#endif //PROJECTS_GENERICRUBIKSCUBE_H
